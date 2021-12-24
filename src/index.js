4ce059ccbe151d8982bc2069e556cce224aa7c99
botamochi6277 <botamochi6277@icloud.com>
2021-12-25 08:44:58 +0900

    add card

--- src/index.js
+++ src/index.js
@@ -1,16 +1,47 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Button from 'react-bootstrap/Button';
+// import Button from 'react-bootstrap/Button';
+import { Button, Card } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+
+function MyCard(props) {
+    return (
+        <Card style={{ width: props.width }}>
+            <Card.Img variant="top" src={props.src} />
+            <Card.Title>{props.title}</Card.Title>
+        </Card>
+    )
+}
 class Gallery extends React.Component {
+    constructor(props) {
+        super(props);
+        let images = [
+            { title: "example01", src: "http://placeholder.pics/svg/300" },
+            { title: "example02", src: "http://placeholder.pics/svg/300" },
+        ]
+        this.state = {
+            width: "18rem",
+            images: images
+        }
+    }
     render() {
+        const width = this.state.width;
+        const images = this.state.images;
+        const cards = images.map((image, index) =>
+            <li>
+                <MyCard width={width} title={image.title} src={image.src} key={index} />
+            </li>
+        );
+
         return (
             <div className="gallery">
                 <div className="game-info">
                     <div>{/* status */}</div>
-                    <ol>{/* TODO */}</ol>
+                    <ul>
+                        {cards}
+                    </ul>
                 </div>
             </div>
         );
